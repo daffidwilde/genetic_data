009370f6e4b287db5c1e9f3d44deb5325d924c89
Henry Wilde <henrydavidwilde@gmail.com>
2019-01-29 22:17:25 +0000

    Fix error in continuous pdf tests.

--- tests/pdfs/test_continuous.py
+++ tests/pdfs/test_continuous.py
@@ -13,33 +13,33 @@ LIMITS = (
 )
 
 CONTINUOUS = given(
-    alpha_limits=LIMITS,
-    theta_limits=LIMITS,
+    first_limits=LIMITS,
+    second_limits=LIMITS,
     seed=integers(min_value=0, max_value=2 ** 32 - 1),
 )
 
 
 @CONTINUOUS
-def test_gamma_set_param_limits(alpha_limits, theta_limits, seed):
+def test_gamma_set_param_limits(first_limits, second_limits, seed):
     """ Check that a Gamma object can sample its parameters correctly if its
     class attributes are altered. """
 
-    Gamma.param_limits = {"alpha": alpha_limits, "theta": theta_limits}
+    Gamma.param_limits = {"alpha": first_limits, "theta": second_limits}
 
     np.random.seed(seed)
     gamma = Gamma()
-    assert gamma.alpha >= alpha_limits[0] and gamma.alpha <= alpha_limits[1]
-    assert gamma.theta >= theta_limits[0] and gamma.theta <= theta_limits[1]
+    assert gamma.alpha >= first_limits[0] and gamma.alpha <= first_limits[1]
+    assert gamma.theta >= second_limits[0] and gamma.theta <= second_limits[1]
 
 
 @CONTINUOUS
-def test_normal_set_param_limits(mean_limits, std_limits, seed):
+def test_normal_set_param_limits(first_limits, second_limits, seed):
     """ Check that a Normal object can sample its parameters correctly if its
     class attributes are altered. """
 
-    Normal.param_limits = {"mean": mean_limits, "std": std_limits}
+    Normal.param_limits = {"mean": first_limits, "std": second_limits}
 
     np.random.seed(seed)
     normal = Normal()
-    assert normal.mean >= mean_limits[0] and normal.mean <= mean_limits[1]
-    assert normal.std >= std_limits[0] and normal.std <= std_limits[1]
+    assert normal.mean >= first_limits[0] and normal.mean <= first_limits[1]
+    assert normal.std >= second_limits[0] and normal.std <= second_limits[1]
