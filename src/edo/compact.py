ba94cde2ab7ff71e687d33a18a313b275e52078d
Henry Wilde <henrydavidwilde@gmail.com>
2019-01-29 23:41:53 +0000

    Start implementing compact functionality.

--- src/edo/compact.py
+++ src/edo/compact.py
@@ -1,6 +1,24 @@
 """ Function for compacting the search space. """
 
 
+def _get_all_param_values(parents):
+    """ Get all the parent parameter values present in a dictionary. """
+
+    all_param_vals = {}
+    for _, metadata in parents:
+        for column in metadata:
+            col = column.__class__
+            if col not in all_param_vals.keys():
+                all_param_vals[col] = {}
+            for param_name, param_val in column.__dict__.items():
+                try:
+                    all_param_vals[col][param_name] += [param_val]
+                except KeyError:
+                    all_param_vals[col][param_name] = [param_val]
+
+    return all_param_vals
+
+
 def compact_search_space(parents, pdfs, itr, max_iter, compaction_ratio):
     """ Given the current progress of the GA, compact the search space, i.e. the
     parameter spaces for each of the distribution classes in :code:`pdfs`. """
@@ -12,5 +30,15 @@ def compact_search_space(parents, pdfs, itr, max_iter, compaction_ratio):
         return pdfs
 
     compact_factor = 1 - (itr / (compaction_ratio * max_iter))
+    all_param_vals = _get_all_param_values(parents)
 
-    pass
+    for pdf, params in all_param_vals.items():
+        for name, vals in params.items():
+
+            lower, upper = min(vals), max(vals)
+            midpoint = sum(vals) / len(vals)
+            shift = (upper - lower) * compact_factor / 2
+
+            pdf.param_limits[name] = [midpoint - shift, upper + shift]
+
+    return pdfs
