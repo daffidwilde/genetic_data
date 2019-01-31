2ecde06acc51e00d0e8988bafd525ae7f70e0264
Henry Wilde <henrydavidwilde@gmail.com>
2019-01-31 00:09:54 +0000

    Try ../src/edo/

--- docs/conf.py
+++ docs/conf.py
@@ -28,9 +28,7 @@ MOCK_MODULES = ['numpy', 'matplotlib', 'matplotlib.pyplot']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
-sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(0, os.path.abspath('../src/edo/'))
 
 import edo
 
