0569bec4bd086657765edc7cc94ac23e2501acf3
Henry Wilde <henrydavidwilde@gmail.com>
2019-03-19 15:29:55 +0000

    Move to `family` over `parent` attribute.

--- tests/pdfs/test_subtypes.py
+++ tests/pdfs/test_subtypes.py
@@ -15,8 +15,10 @@ def test_make_subtype(cls):
     for key, value in vars(subtype).items():
         if key in vars(cls) and key != "subtypes":
             assert getattr(cls, key) == value
+
     assert subtype.__repr__ is cls.__repr__
 
     sub = subtype()
+    assert sub.family == cls
     assert isinstance(sub, subtype)
     assert not isinstance(sub, cls)
