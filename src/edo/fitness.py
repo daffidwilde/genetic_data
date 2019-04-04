81700d1377328d72964374427b4537c76470a22c
Henry Wilde <henrydavidwilde@gmail.com>
2019-04-04 18:56:21 +0100

    Allow for caching in fitness calculation.

--- src/edo/fitness.py
+++ src/edo/fitness.py
@@ -1,13 +1,22 @@
 """ Fitness-related functions. """
 
 
-def get_fitness(fitness, population, fitness_kwargs=None):
-    """ Return the fitness score of each individual in a population. """
+def get_fitness(dataframe, fitness, cache, fitness_kwargs=None):
+    """ Return the fitness score of the individual. """
 
-    if fitness_kwargs:
-        return [
-            fitness(individual.dataframe, **fitness_kwargs)
-            for individual in population
-        ]
+    if repr(dataframe) not in cache:
+        if fitness_kwargs:
+            cache[repr(dataframe)] = fitness(dataframe, **fitness_kwargs)
+        else:
+            cache[repr(dataframe)] = fitness(dataframe)
 
-    return [fitness(individual.dataframe) for individual in population]
+    return cache[repr(dataframe)]
+
+
+def get_population_fitness(population, fitness, cache, fitness_kwargs=None):
+    """ Return the fitness of each individual in the population. """
+
+    return [
+        get_fitness(individual.dataframe, fitness, cache, fitness_kwargs)
+        for individual in population
+    ]
