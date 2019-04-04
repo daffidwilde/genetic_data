06a03373664da49a5f05cdc54705afe6bfb3bf60
Henry Wilde <henrydavidwilde@gmail.com>
2019-04-04 20:14:41 +0100

    Dump commit. Introducing global caching for dask.

--- src/edo/fitness.py
+++ src/edo/fitness.py
@@ -1,8 +1,15 @@
 """ Fitness-related functions. """
 
+import dask
+import edo
 
-def get_fitness(dataframe, fitness, cache, fitness_kwargs=None):
+
+@dask.delayed
+def get_fitness(dataframe, fitness, fitness_kwargs=None):
     """ Return the fitness score of the individual. """
+
+    global cache
+    cache = edo.cache
 
     if repr(dataframe) not in cache:
         if fitness_kwargs:
@@ -13,10 +20,19 @@ def get_fitness(dataframe, fitness, cache, fitness_kwargs=None):
     return cache[repr(dataframe)]
 
 
-def get_population_fitness(population, fitness, cache, fitness_kwargs=None):
-    """ Return the fitness of each individual in the population. """
+def get_population_fitness(population, fitness, processes=None, fitness_kwargs=None):
+    """ Return the fitness of each individual in the population. This can be
+    done in parallel by specifying a number of cores to use for independent
+    processes. """
 
-    return [
-        get_fitness(individual.dataframe, fitness, cache, fitness_kwargs)
+    tasks = (
+        get_fitness(individual.dataframe, fitness, fitness_kwargs)
         for individual in population
-    ]
+    )
+
+    if processes is None:
+        out = dask.compute(*tasks, scheduler="single-threaded")
+    else:
+        out = dask.compute(*tasks, num_workers=processes)
+
+    return out
--- tests/unit/test_fitness.py
+++ tests/unit/test_fitness.py
@@ -1,5 +1,6 @@
 """ Test(s) for the calculating of population fitness. """
 
+import edo
 from edo.fitness import get_fitness, get_population_fitness
 from edo.pdfs import Normal, Poisson, Uniform
 from edo.individual import create_individual
@@ -14,12 +15,15 @@ def test_get_fitness(row_limits, col_limits, weights):
     """ Create an individual and get its fitness. Then verify that the fitness
     is of the correct data type and has been added to the cache. """
 
-    cache = {}
+    global cache
+    cache = edo.cache
+    cache.clear()
+
     families = [Normal, Poisson, Uniform]
     individual = create_individual(row_limits, col_limits, families, weights)
     dataframe = individual.dataframe
 
-    fit = get_fitness(dataframe, trivial_fitness, cache)
+    fit = get_fitness(dataframe, trivial_fitness).compute()
     assert repr(dataframe) in cache
     assert isinstance(fit, float)
 
@@ -30,13 +34,16 @@ def test_get_fitness_kwargs(row_limits, col_limits, weights):
     verify that the fitness is of the correct data type and has been added to
     the cache. """
 
-    cache = {}
+    global cache
+    cache = edo.cache
+    cache.clear()
+
     fitness_kwargs = {"arg": None}
     families = [Normal, Poisson, Uniform]
     individual = create_individual(row_limits, col_limits, families, weights)
     dataframe = individual.dataframe
 
-    fit = get_fitness(dataframe, trivial_fitness, cache, fitness_kwargs)
+    fit = get_fitness(dataframe, trivial_fitness, fitness_kwargs).compute()
     assert repr(dataframe) in cache
     assert isinstance(fit, float)
 
@@ -47,12 +54,15 @@ def test_get_population_fitness(size, row_limits, col_limits, weights):
     is of the correct data type and size, and that they have each been added to
     the cache. """
 
-    cache = {}
+    global cache
+    cache = edo.cache
+    cache.clear()
+
     families = [Normal, Poisson, Uniform]
     population = create_initial_population(size, row_limits, col_limits,
             families, weights)
 
-    pop_fit = get_population_fitness(population, trivial_fitness, cache)
+    pop_fit = get_population_fitness(population, trivial_fitness, 2)
     assert len(pop_fit) == size
     for ind, fit in zip(population, pop_fit):
         assert repr(ind.dataframe) in cache
