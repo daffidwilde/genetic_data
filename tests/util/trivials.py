d246acd71b99f3966cf9f72a05e8bd5eebe2911b
Henry Wilde <henrydavidwilde@gmail.com>
2019-06-10 16:40:43 +0100

    Final clean up. Keeping old tests for scrap.

---
+++ tests/util/trivials.py
@@ -0,0 +1,17 @@
+""" A collection of trivial objects for use in tests. """
+
+import numpy as np
+import pandas as pd
+
+
+def trivial_fitness(individual, arg=None):
+    """ A fitness function. """
+    assert isinstance(individual, pd.DataFrame)
+    assert arg is None
+    return 0.0
+
+
+def trivial_stop(pop_fitness):
+    """ A stopping condition. """
+    assert isinstance(pop_fitness, list)
+    return False
