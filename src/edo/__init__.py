a1b2f95dd9c3a96fdaae2b19694143c9530656ba
Henry Wilde <henrydavidwilde@gmail.com>
2020-07-25 01:38:23 +0100

    Format codebase with black, flake8 and isort.

--- src/edo/__init__.py
+++ src/edo/__init__.py
@@ -2,3 +2,5 @@ from .optimiser import DataOptimiser
 from .version import __version__
 
 cache = {}
+
+__all__ = ["DataOptimiser", "__version__", "cache"]
--- src/edo/families/__init__.py
+++ src/edo/families/__init__.py
@@ -8,3 +8,15 @@ all_families = [Bernoulli, Gamma, Normal, Poisson, Uniform]
 
 continuous_families = [fam for fam in all_families if fam.dtype == "float"]
 discrete_families = [fam for fam in all_families if fam.dtype == "int"]
+
+__all__ = [
+    "Bernoulli",
+    "Distribution",
+    "Gamma",
+    "Normal",
+    "Poisson",
+    "Uniform",
+    "all_families",
+    "continuous_families",
+    "discrete_families",
+]
